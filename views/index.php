b4335477ff2a9ef6aff031d46cb8dc6882ad392c
ZainabMadann <zainabmadann@gmail.com>
2025-03-20 02:11:42 +0300

    home page done

--- views/index.php
+++ views/index.php
@@ -1,7 +1,7 @@
 <?php require('../views/template/header.phtml'); ?>
 <?php
-// include __DIR__ . '/../views/home.phtml';
-include __DIR__ . '/../views/ownerProfile.phtml';
+include __DIR__ . '/../views/home.phtml';
+// include __DIR__ . '/../views/ownerProfile.phtml';
 
 ?>
 <?php require('../views/template/footer.phtml'); ?>
