6abaebabc8378da3b10f23caf5db96c8dd637354
ZainabMadann <zainabmadann@gmail.com>
2025-03-20 03:03:47 +0300

    payment page , booking page , explore page

--- views/index.php
+++ views/index.php
@@ -1,6 +1,6 @@
 <?php require('../views/template/header.phtml'); ?>
 <?php
-include __DIR__ . '/../views/home.phtml';
+include __DIR__ . '/../views/payment.phtml';
 // include __DIR__ . '/../views/ownerProfile.phtml';
 
 ?>
