29ce40765e267429849d186045f9f17ea7c16b86
ZainabMadann <zainabmadann@gmail.com>
2025-03-23 13:18:12 +0300

    trying link css

--- views/index.php
+++ views/index.php
@@ -1,6 +1,6 @@
 <?php require('../views/template/header.phtml'); ?>
 <?php
-include __DIR__ . '/../views/adminProfile.phtml';
+include __DIR__ . '/../views/home.phtml';
 // include __DIR__ . '/../views/ownerProfile.phtml';
 
 ?>
